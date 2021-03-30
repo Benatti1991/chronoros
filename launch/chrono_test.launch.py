5f8c0db0137a2706d12a2c97e88605b71281dbd5
Benatti1991 <simone.benatti@studenti.unipr.it>
2021-03-30 21:55:26 +0200

    Modified launch script to accomodate command line args

--- launch/chrono_test.launch.py
+++ launch/chrono_test.launch.py
@@ -1,16 +1,28 @@
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 import launch_ros.actions
-
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-#    clp = get_args()
-    return LaunchDescription([
-        launch_ros.actions.Node(
-            package='chronoros',
-            node_executable='chrono_test_node',
-            output='screen',
-            parameters=[
-                {"my_parameter": "chimichanga"}
-            ]),
-    ])
+    vals = ['chimichanga']
+    my_parameter = LaunchConfiguration('my_parameter', default=vals)
+
+    declare_my_parameter_cmd = DeclareLaunchArgument(
+        'model_files',
+        default_value=vals,
+        description='PDDL Model file')
+
+    # Specify the actions
+    domain_expert_cmd = Node(
+        package='chronoros',
+        node_executable='chrono_test_node',
+        output='screen',
+        parameters=[{'my_parameter': my_parameter}])
+
+    ld = LaunchDescription()
+    ld.add_action(declare_my_parameter_cmd)
+    ld.add_action(domain_expert_cmd)
+
+    return ld
